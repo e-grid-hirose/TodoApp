0ad878786d1fd38f49233750ace2956153b03c65
runa <hirose.runa@e-grid.co.jp>
2024-08-08 09:37:07 +0900

    commit

---
+++ springboottodo/src/main/java/com/example/SpringBootTodo/repository/TodoRepository.java
@@ -0,0 +1,11 @@
+package com.example.SpringBootTodo.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.example.SpringBootTodo.model.Todo;
+
+@Repository
+public interface TodoRepository extends JpaRepository<Todo,Long>{
+	
+}
